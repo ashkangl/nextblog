f76cea66f598e0edf712f77ab8440a4ef42ee294
Ashkan Golzad <ashkan72golzad@gmail.com>
2024-11-14 11:56:57 +0330

    ready to deploy

--- src/app/addPosts/page.jsx
+++ src/app/addPosts/page.jsx
@@ -1,10 +1,4 @@
-import Posts from "@/components/posts/Posts";
-
-export const metadata = {
-    title: "Add Posts On Blog",
-    description: "This is description for next js blog",
-    keywords: "upload post, upload image, upload tags, add posts, next js, react js, next js blog, vercel"
-}
+import Posts from "../../components/posts/Posts";
 
 const Page = () => {
     return(
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,12 @@ import Footer from "@/components/general/Footer";
 import Header from "@/components/general/Header";
 import "./globals.css"
 
+export const metadata = {
+  title: "NEXT JS BLOG SAMPLE",
+  description: "This is description for next js blog",
+  keywords: "upload post, upload image, upload tags, add posts, next js, react js, next js blog, vercel"
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
